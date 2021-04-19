76e00d26b54aa1024ab1592144dd461c4bffd847
BoraNy <49944183+BoraNy@users.noreply.github.com>
2021-04-19 13:31:48 +0700

    Update InfoMenu.ino

--- InfoMenu.ino
+++ InfoMenu.ino
@@ -48,7 +48,7 @@ void InfoMenu(void) {
   display.setCursor(64, 38); display.print(F("28%"));
   display.setCursor(1, 47);  display.print(F("EEPROM")); 
   display.setCursor(64, 47); display.print(F("1.70%"));
-  display.setCursor(1, 56);  display.print(F("Version"));
+  display.setCursor(1, 56);  display.print(F("VERSION"));
   display.setCursor(64, 56); display.print(F("19.042021"));
   display.display();
   display.clearDisplay();
